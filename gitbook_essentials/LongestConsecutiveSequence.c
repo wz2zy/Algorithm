23225afbe1e3da7b5d957c0af711630dcde1d11d
zY <2639424571@qq.com>
2019-11-15 18:09:46 +0800

    【gitbook】【算法珠玑】【第三个问题】【获取没排序序列中最长连续子列长度】

---
+++ gitbook_essentials/LongestConsecutiveSequence.c
@@ -0,0 +1,66 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+#define ElementType int
+
+#define BITSPERWORD 32
+#define SHIFT		5
+#define MASK		0x1F
+#define N			10000000
+
+unsigned int bitmap[1 + N / BITSPERWORD] = { 0 };
+
+void set_bit(unsigned long bit) {
+	bitmap[bit >> SHIFT] |=  (1 << (bit & MASK));
+}
+
+void clr_bit(unsigned long bit) {
+	bitmap[bit >> SHIFT] &= ~(1 << (bit & MASK));
+}
+
+bool get_bit(unsigned long bit) {
+	return bitmap[bit >> SHIFT] & (1 << (bit & MASK));
+}
+
+size_t GetLongestConsecutiveSequence(ElementType nums[], size_t length)
+{
+	if (length <= 1)
+		return length;
+
+	for (size_t i = 0; i < length; ++i) {
+		set_bit(nums[i]);
+	}
+
+	// 验证写入是否正确
+	//for (size_t bit = 0; bit < N; ++bit) {
+	//	if (get_bit(bit)) {
+	//		printf("%dst bit is been setted...\n", bit);
+	//	}
+	//}
+
+	size_t curlength = 1;
+	size_t maxlength = 0;
+	for (size_t bit = 1; bit < N; ++bit) {
+		if (get_bit(bit) && get_bit(bit - 1)) {
+			++curlength;
+		}
+		else
+		{
+			if (curlength > maxlength)
+				maxlength = curlength;
+
+			curlength = 1;
+		}
+	}
+
+	return maxlength;
+}
+
+void main(void)
+{
+	ElementType nums[] = { 100, 4, 200, 1, 3, 2 };
+
+	size_t length = GetLongestConsecutiveSequence(nums, sizeof(nums) / sizeof(ElementType));
+
+	printf("Max consecutive sequence is %d\n", length);
+}
---
+++ gitbook_essentials/LongestConsecutiveSequence.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <set>
+#include <iterator>
+
+// Longest Consecutive Sequence
+
+#define ElementType int
+
+size_t GetLongestConsecutiveSequence(ElementType nums[], size_t length)
+{
+	if (length <= 1)
+		return length;
+	
+	std::set<ElementType> set;
+
+	for (size_t i = 0; i < length; ++i) {
+		set.insert(nums[i]);
+	}
+
+	size_t lengthest = 0;
+
+	for (ElementType num : set) {
+		size_t length = 1;
+
+		for (ElementType preNum = num - 1; set.find(preNum) != set.end(); --preNum) {
+			// 向前找
+			set.erase(preNum);
+			++length;
+		}
+
+		for (ElementType nextNum = num + 1; set.find(nextNum) != set.end(); ++nextNum) {
+			// 向后找
+			set.erase(nextNum);
+			++length;
+		}
+
+		lengthest = lengthest >= length ? lengthest : length;
+	}
+
+	return lengthest;
+}
+
+void main(void)
+{
+	size_t length = 0;
+
+	// ElementType nums[] = { 1,1,1,2,2,3 };
+	// ElementType nums[] = { 1,1,1,2,2,3,4,4,4,4,5,5,5,6,7,7,7,8,8,8,9,9 };
+	ElementType nums[] = { 100,4,200,1,3,2 };
+
+	size_t lengthest = GetLongestConsecutiveSequence(nums, sizeof(nums) / sizeof(ElementType));
+
+	printf("lengthest = %d", lengthest);
+}
